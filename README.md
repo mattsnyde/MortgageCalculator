fb77364802e189c42121767f77148f79349fcf83
mattsnyde <matthewx.snyder@gmail.com>
2021-06-08 18:55:42 -0700

    Mortgage Calculator finished

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-Fix the border bottom when active, currenctly changing to white. This is for the monthly payment section not the mortgage calculator section.
+**Mortgage Calculator Breakdown**
+----------------------------------------------------------------------------------------
+Summary of application: This is a simple mortgage calculator which caculates the monthly payment based on 6 factors.
+    1) Home Price   2) Down payment     3) Loan Type (Yrs of loan)     4) Mortgage Interest rate        5) Property Taz $/Yr     6) Home Insurance $/Yr.
+
+The mortgage is calculated by the formula: M = P[r(1+r)^n/((1+r)^n)-1)] which is only the principal and the interest, then I add on the Annual property tax /  12
+as well as the annual home insurance / 12. 
+
+Then I break down the combined monthly payment into 4 categories which are shown in the first page which is Principal Amount (Monthly), Property Tax (Monthly), Home Insurance (Monthly), and Interest Amount (Monthly).
+
+Using Chart.js library I was able to construct a doughnut graph which represents a breakdown of the users monthly payment by %'s.
+
+When the user clicks calculate it formats everything into dollar currency format. 
+
+The second page contains a table of estimates we provide to the user. 
+    The table contains, home price, home down paymwent, loan type, mortgage interest rate, annual property tax, annual home owner insurance, monthly interest, monthly home insurance, combined monthly payment, estimate #. 
+
+    This table is updated by using localStorage, the localStorage is an empty array and then I take all of the variables listed on line 16, put them in an array and push it to the localStorage so I am nesting arrays.
+
+    The data is filled within the table automatically using nested for loops to populate the cells with the data inside of localStorage. 
+
+If the user wishes they can clear their localStorage using the clear table btn. 
+
+
+*What I would change*
+--------------------------------------
+1. Table updates automatically without having to refresh the page
+2. Make it more responsive overall
+3. come up with a better theme/colors
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let myChart1 = document.querySelector('#myChart').getContext('2d') //we get the chart canvas that we created in html and we use 'getContext because we are creating 2 dimensional charts, nothing 3D. 
+let labels1 = ['Monthly Interest', 'Monthly Principal', 'Monthly Property Tax', 'Monthly Home Insurance']
+let colors1 = ['#e9d30b', 'rgb(126, 228, 126)', 'red', 'orange']
+ 
 const homePriceInput = document.querySelector('#homePriceInput')
 const homePrice = parseInt(homePriceInput.placeholder)
 const downPaymentInput = document.querySelector('#downPaymentInput')
@@ -14,20 +18,18 @@ const loanTypeSelection = document.querySelector('#loanTypeSelection')
 const monthlyDollarPropertyTaxHolder = document.querySelector('#homePropertyTaxInput')
 const monthlyDollarHomeOwnerInsurance = document.querySelector('#homeOwnerInsuranceInput')
 
+const monthlyPaymentLbl = document.querySelector('.monthlyPaymentLbl');
 
 const calculate = document.querySelector('#calculate')
 const form = document.querySelector('#form')
+const calcContainer = document.querySelector('#calc-container')
+const form2 = document.querySelector('.form2')
+const table = document.querySelector('.tableInformation')
 
-const rightDivPrincpial = document.querySelector('.right')
-const leftDivInterest = document.querySelector('.progress')
-const entireCircle = document.querySelector('.circle')
-const barCircle = document.querySelector('.bar')
-const innerCircle = document.querySelector('.numb')
-
+const clearTable = document.querySelector('#clearTbl')
 
 const MONTHS_IN_YEAR = 12;
 let dollars = Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
- 
  
 function calculating(){
     form.addEventListener('click', (e) => { //When clicking the form we want to prevent the default acction from happening, the default action when clicking a form will reset what you are trying to acheive
@@ -44,24 +46,22 @@ function calc(){
         let annualPropertyTax = parseFloat(monthlyPropertyTaxHolder.value)
         let annualHomeOwnerInsurance = parseFloat(monthlyHomeOwnerInsurance.value)
         let loanType = 0;
+
         //for lines 24-36 depdent on the drop down the user selects is the amount of years that they will have the loan for. Ranges from 10, 15, 20, 30yrs
          if(loanTypeSelection.value === 'twentyYears'){ 
-             console.log('20 yr')
             loanType = 20;
         }else if(loanTypeSelection.value === 'thirtyYears'){
-            console.log('30 yr')
             loanType = 30;
         }else if(loanTypeSelection.value === 'tenYears'){
-            console.log('ten year')
             loanType = 10;
         }else if(loanTypeSelection.value === 'fifteenYears'){
-            console.log('fifteen year')
             loanType = 15;
         }
+
         let monthlyPropertyTax = Math.round(annualPropertyTax / 12)
-        console.log(monthlyPropertyTax, 'monthly property tax')
+
         let monthlyHomeInsurance = Math.round(annualHomeOwnerInsurance / 12)
-        console.log(monthlyHomeInsurance, 'monthly home owner insurance')
+
         let rate = mortgageInterestRate / MONTHS_IN_YEAR; //rate is represented by interest divided by months in year
         let principal = homePrice - homeDownPayment; //principal of loan is found by subtracting cost of home from downpayment
         let numOfPayment = MONTHS_IN_YEAR * loanType; //num of payments needed is founded by multiplying type of loan by months in year
@@ -69,29 +69,69 @@ function calc(){
         let monthlyInterestPayed = (((loanType * MONTHS_IN_YEAR) * monthlyPayment) - (homePrice-homeDownPayment)) / (loanType * MONTHS_IN_YEAR) //Calculate the amount of interest the person is paying each monthly payment
         let monthlyPrincpialPayed = monthlyPayment - monthlyInterestPayed //Amount in princpial the person is paying
         
-        console.log('combined monthly payment', monthlyPayment + monthlyPropertyTax + monthlyHomeInsurance)
+        let combiendMonthlyPayment =  monthlyPayment + monthlyPropertyTax + monthlyHomeInsurance
         monthlyInterestHolder.value = monthlyInterestPayed //set value of input field to monthly interest payment
         monthlyPrincipalHolder.value = monthlyPrincpialPayed //set value of princpial field to monthly princpial payment
 
-         monthlyDollarPropertyTaxHolder.value = monthlyPropertyTax; //set value of in
+        monthlyPaymentLbl.innerHTML = 'Monthly Payment: ' + dollars.format(combiendMonthlyPayment)
+
+        monthlyDollarPropertyTaxHolder.value = monthlyPropertyTax; //set value of in
         monthlyDollarHomeOwnerInsurance.value= monthlyHomeInsurance;
 
-        monthlyInterestPercent = (monthlyInterestPayed / monthlyPayment) * 100; //Represents the percentage of your monthly payment that goes to interest
-        monthlyPrincipalPercent = (monthlyPrincpialPayed / monthlyPayment) * 100; //Represents the percentage of your monthly payment that goes to percent
-        
-        let monthlyInterestDegrees =  (monthlyInterestPercent / 100) * 360 //gets degree of interest by dividing interest by 100 and multiplying by, degree of principal is also done this way
-        let monthlyPrincipalDegress = (monthlyPrincipalPercent / 100)*360
-        if(monthlyPrincipalDegress > monthlyInterestDegrees){ //There is a problem with the circle where it does not complete all the way so we need to manually change the color based on which is greater, interest or princpial
-            entireCircle.style.backgroundColor = 'rgb(126, 228, 126)'
-            barCircle.style.backgroundColor = 'rgb(126, 228, 126)'
-        }else if(monthlyInterestDegrees > monthlyPrincipalDegress){
-            entireCircle.style.backgroundColor = '#e9d30b'
-            barCircle.style.backgroundColor = '#e9d30b'
+        let monthlyInterestPercent = (monthlyInterestPayed / combiendMonthlyPayment) * 100; //Represents the percentage of your monthly payment that goes to interest //monthlyPayment
+        let monthlyPrincipalPercent = (monthlyPrincpialPayed / combiendMonthlyPayment) * 100; //Represents the percentage of your monthly payment that goes to percent //monthlyPayment
+        let monthlyPropertyTaxPercent = (monthlyPropertyTax / combiendMonthlyPayment) * 100
+        let monthlyHomeInsurancePercent = (monthlyHomeInsurance / combiendMonthlyPayment) * 100;
+
+        //Get new data from the input of the user and the output that we calculate for them
+        let informatics = [homePrice, homeDownPayment, loanType, mortgageInterestRate, annualPropertyTax, annualHomeOwnerInsurance, monthlyInterestPayed, monthlyPrincpialPayed, monthlyPropertyTax, monthlyHomeInsurance, combiendMonthlyPayment]
+        let new_data = informatics; //assign our array to a variable called new_data
+    
+        if(localStorage.getItem('information')==null){ //When first setting up a localStorage it has a value of null so we are able to initialize the localStorage with whatever we want in this case an empty array
+            localStorage.setItem('information', '[]')
         }
-        rightDivPrincpial.style.transform = `rotate(${monthlyPrincipalDegress}deg)` //rotate the circle by degree amount
-        leftDivInterest.style.transform =`rotate(${monthlyInterestDegrees}deg)`
-        innerCircle.innerHTML = dollars.format(monthlyPayment)
-     })
+
+         let old_data = JSON.parse(localStorage.getItem('information')) //Parse the old data already inside of the localStorage 
+         old_data.push(new_data) //Push the new Data we colelcted from user and our computations and push it inside of the existing array from localStorage
+         localStorage.setItem('information', JSON.stringify(old_data)) //Set localStorage back to old_data containing the new information        
+
+        let data1 = [monthlyInterestPercent, monthlyPrincipalPercent, monthlyPropertyTaxPercent, monthlyHomeInsurancePercent] //Holds the data set we are using for the chart
+        let chart1 = new Chart(myChart1, { //we are creating a new chart which is a doughnut chart
+            type: 'doughnut', 
+            data: {
+                labels: labels1, //labels is the labels1 array
+                datasets: [{
+                    data: data1, //data is the data1 array
+                    backgroundColor: colors1,
+                    borderColor: 'none'
+                }]
+            },
+            options: {
+                title:{
+                    text: 'Monthly Payment', //Display tthe title
+                    display: true
+                },
+                layout:{
+                    padding:{
+                        right: 0, left: 0, bottom: 0, top: 0,
+                    }
+                },
+                responsive: true, //responsive graph
+                plugins:{
+                    legend:{
+                        font:{
+                            size: 6 //make font of legend smaller
+                        },
+                        labels:{ 
+                            font:{
+                                size: 6 //make font of labels smaller
+                            }
+                        }
+                    }
+                }
+            }
+        })
+    })
 }
 calc();
 //interest paid on loan overall, principal loan amount * interet rate * number of years in term
@@ -104,10 +144,7 @@ function formatUserInfo(){
         downPaymentInput.value = dollars.format(downPaymentInput.value)
         monthlyInterestHolder.value = dollars.format(monthlyInterestHolder.value)
         monthlyPrincipalHolder.value = dollars.format(monthlyPrincipalHolder.value)
-
-    //         const monthlyPropertyTaxHolder = document.querySelector('#propertyTaxInput')
-    // const monthlyHomeOwnerInsurance = document.querySelector('#homeInsuranceInput')
-        monthlyPropertyTaxHolder.value = dollars.format(monthlyPropertyTaxHolder.value)
+         monthlyPropertyTaxHolder.value = dollars.format(monthlyPropertyTaxHolder.value)
         monthlyHomeOwnerInsurance.value  = dollars.format(monthlyHomeOwnerInsurance.value)
         monthlyDollarPropertyTaxHolder.value = dollars.format(monthlyDollarPropertyTaxHolder.value)
         monthlyDollarHomeOwnerInsurance.value =dollars.format(monthlyDollarHomeOwnerInsurance.value)
@@ -115,3 +152,89 @@ function formatUserInfo(){
 }
 formatUserInfo();
  
+ 
+
+const bttn = document.querySelector('#btn')
+function hideMainPage(){ //When I click the btn on the first page I want to bury this page behind the second page so it is not visible whatso ever
+    bttn.addEventListener('click', () => {
+        form.style.opacity = 0;
+        form.style.zIndex = -100
+        calcContainer.style.opacity = 0;
+        calcContainer.style.zIndex = -100;
+        console.log('hihhh')
+        form2.style.opacity = 1;
+    })
+}
+hideMainPage();
+
+ 
+const bttn2 = document.querySelector('#btn2') //When I click the btn on the second page I want to hide the second page behind the first page so it is not visible whatsoever
+function hideSecondPage(){
+    bttn2.addEventListener('click', () => {
+        form2.style.opacity = 0;
+        calcContainer.style.opacity = 1;
+        form.style.opacity = 1;
+        form.style.zIndex = 100;
+        calcContainer.style.zIndex = 100;
+    })
+}
+hideSecondPage();
+let arrayCounter =0;
+let stack = JSON.parse(localStorage.getItem('information')) //Pass all parsed infrom into stack which is a nested array
+for(let i = 0; i < stack.length; i++){ //loop through stack to see all information currently inside of the localStorage
+    for(let j = 0; j < stack[i].length; j++){
+        console.log(stack[i][j])
+        if(j % 9 === 0 && j !==0){
+                 ++arrayCounter;
+                console.log('this is the row', arrayCounter)
+        }
+    }
+}
+
+ function generate_table(){
+     //create table and tbody elements
+      let tblBody = document.createElement('tbody')
+
+     //create the cells
+     for(let i = 0; i < stack.length; i++){
+         //create table row
+         console.log('this is i', i)
+         let row = document.createElement('tr')
+         for(let j = 0; j < 12; j++){
+             //create td element and text node make text node contets of td and put td at the end of table row
+             let cell = document.createElement('td')
+            //  let cellText = the localStorage item, localStorage was set to stack which is a nested array that can be traversed using numbers here.
+            if(j === 11){ // j represents how long each row is, when j is 10 we want to set cellText to i + 1 and append that to the cell of 10. Else for all of the other cells we would like to just append the information from stack[i][j] which contains all of the information inside of localStorage
+                console.log('j is 10')
+                let cellText = document.createTextNode(i + 1)
+                cell.append(cellText)
+            }else if(j === 0 || j === 1 || j === 4 || j === 5 || j === 6 || j === 7 || j===8 || j === 9 || j === 10) { //The information inside of these cells are numbers and we want to format them as such with dollar signs and decimal places
+                let cellText = document.createTextNode(dollars.format(stack[i][j]))
+                cell.appendChild(cellText)
+            }else{
+                let cellText = document.createTextNode(stack[i][j])
+                cell.appendChild(cellText)
+            }
+             row.appendChild(cell)
+
+         }
+         //add the row to the end of table body
+         tblBody.appendChild(row)
+     }
+     //put tbody inside of the table
+     table.appendChild(tblBody)
+     //append table into form2
+     form2.appendChild(table)
+     //set the border attrbiute of table to 2
+     table.setAttribute('border', '2')
+ }
+//  generate_table()
+generate_table();
+
+function clearStrg(){ //Clear the storage on click
+    clearTable.addEventListener('click', () => {
+        localStorage.clear();
+        console.log('cleared')
+    })
+}
+clearStrg();
